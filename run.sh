7606f70d743d1e577a8ecaa3ccff4b1c725563ba
David Robles <drobles@gmail.com>
2016-12-14 00:18:35 -0500

    Changes based on review

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm proposal.aux proposal.dvi proposal.log proposal.pdf proposal.ps;
 # latex proposal && bibtex proposal && latex proposal && latex proposal && dvips proposal.dvi && ps2pdf proposal.ps
-pdflatex proposal && bibtex proposal && pdflatex proposal && pdflatex proposal && cp proposal.pdf /home/django/django_project/django_project/static/
+pdflatex proposal && bibtex proposal && pdflatex proposal && pdflatex proposal && cp proposal.pdf /home/drobles/projects/aibattle/aibattle/static/
